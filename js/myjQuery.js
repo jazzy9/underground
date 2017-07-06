c402759d57a44b379ba65faf3ff75912554f3482
apsiapsi <hello@apsara.ch>
2017-07-06 22:22:16 +0100

    responsive now

--- js/myjQuery.js
+++ js/myjQuery.js
@@ -1,5 +1,8 @@
-
+// A $( document ).ready() block.
  $(document).ready(function(){
       $('.bxslider').bxSlider()
-  
         });
+ 	$('.bigger').mouseover(function() {
+  		var fs = $('.bigger').css('fontSize');
+  		$('.bigger').css('fontSize', (parseInt(fs, 10) + 1) + 'px');
+	});
