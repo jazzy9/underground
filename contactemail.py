1695708e8098a05048ebaf426d50b4d2cee0f83f
pipbrown <pip.c.brown@gmail.com>
2019-05-23 20:21:17 +0100

    Update contactemail.py
    import requests and calling function

--- contactemail.py
+++ contactemail.py
@@ -1,3 +1,4 @@
+import requests
 def send_simple_message():
     return requests.post("https://api.mailgun.net/v3/sandboxd8603bb5714a4eb18e58db705e9688e8.mailgun.org/messages",
         auth=("api","93fdb1c517a7bd307ed8e12cee162f8b-e566273b-ba4c284f"),
@@ -5,3 +6,4 @@ def send_simple_message():
                 "to": ["bar@example.com"],
                 "subject": "Hello",
                 "text": "Testing some Mailgun awesomness!"})
+send_simple_message
