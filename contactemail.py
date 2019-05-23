65a83398048e816ff0538f86472a4e9f58ed1ee8
Sunaina Parvathi <s.p.kosarajs@lse.ac.uk>
2019-05-23 20:13:20 +0100

    Update contactemail.py
    With working mailgun

--- contactemail.py
+++ contactemail.py
@@ -1,9 +1,7 @@
-import requests
 def send_simple_message():
-	return requests.post(
-		"https://api.mailgun.net/v3/sandboxe0fdd16ae4ed4f1baf0edcfe25b2e498.mailgun.org/messages",
-		auth=("api", "068a1478ef2321af6b0bee1f6601e61a-e566273b-437890da"),
-		data={"from": "Excited User <mailgun@https://app.mailgun.com/app/sending/domains/sandboxe0fdd16ae4ed4f1baf0edcfe25b2e498.mailgun.org>",
-			"to": ["bar@example.com", "pip.c.brown@gmail.com"],
-			"subject": "Hello",
-			"text": "Testing some Mailgun awesomness!"})
+    return requests.post("https://api.mailgun.net/v3/sandboxd8603bb5714a4eb18e58db705e9688e8.mailgun.org/messages",
+        auth=("api","93fdb1c517a7bd307ed8e12cee162f8b-e566273b-ba4c284f"),
+        data={"from": "Excited User <mailgun@sandboxd8603bb5714a4eb18e58db705e9688e8.mailgun.org>",
+                "to": ["bar@example.com"],
+                "subject": "Hello",
+                "text": "Testing some Mailgun awesomness!"})
